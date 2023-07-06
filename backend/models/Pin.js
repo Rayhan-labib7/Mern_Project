fcdc03a82deecaca481989be68307fcad342cf0c
Rayhan Labib <rayhanlabib7@gmail.com>
2023-07-06 10:37:43 +0600

    Travel app share

--- backend/models/Pin.js
+++ backend/models/Pin.js
@@ -1,38 +1,36 @@
 const mongoose = require("mongoose");
 
-const PinSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
+const PinSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
     },
-    title: {
-      type: String,
-      required: true,
-      min: 3,
-      max: 60,
+    title:{
+        type:String,
+        require:true,
+        min:3
     },
-    desc: {
-      type: String,
-      required: true,
-      min: 3,
+    desc:{
+        type:String,
+        require:true,
+        min:3
     },
-    rating: {
-      type: Number,
-      required: true,
-      min: 0,
-      max: 5,
+    rating:{
+        type:Number,
+        require:true,
+        min:0,
+        max:5,
     },
-    long: {
-      type: Number,
-      required: true,
+    lat:{
+        type:Number,
+        require:true,
     },
-    lat: {
-      type: Number,
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+    long:{
+        type:Number,
+        require:true,
+    }
+},
+{timestamps:true}
+)
 
-module.exports = mongoose.model("Pin", PinSchema);
+module.exports = mongoose.model("Pin",PinSchema);
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,58 +1,51 @@
-import { Cancel, Room } from "@material-ui/icons";
-import axios from "axios";
-import { useRef, useState } from "react";
-import "./register.css";
-
-export default function Register({ setShowRegister }) {
-  const [success, setSuccess] = useState(false);
-  const [error, setError] = useState(false);
-  const usernameRef = useRef();
-  const emailRef = useRef();
-  const passwordRef = useRef();
-
+import Room from '@mui/icons-material/Room'
+import axios from 'axios';
+import React, { useRef, useState } from 'react'
+import './register.css'
+import Cancel from '@mui/icons-material/Cancel';
+export default function Register({setShowRegister}) {
+  const [success,setSuccess]=useState(false);
+  const [error,setError]=useState(false);
+  const nameRef=useRef();
+  const emailRef=useRef();
+  const passwordRef=useRef();
+ 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const newUser = {
-      username: usernameRef.current.value,
-      email: emailRef.current.value,
-      password: passwordRef.current.value,
-    };
-
-    try {
-      await axios.post("/users/register", newUser);
-      setError(false);
-      setSuccess(true);
-    } catch (err) {
-      setError(true);
+    const newUser={
+      username:nameRef.current.value,
+      email:emailRef.current.value,
+      password:passwordRef.current.value,
     }
-  };
+    
+    try{
+       await axios.post('/users/register',newUser);
+       setError(false)
+       setSuccess(true);
+    }catch(err){
+      setError(true)
+    }
+    
+  }
   return (
-    <div className="registerContainer">
-      <div className="logo">
-        <Room className="logoIcon" />
-        <span>LamaPin</span>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input autoFocus placeholder="username" ref={usernameRef} />
-        <input type="email" placeholder="email" ref={emailRef} />
-        <input
-          type="password"
-          min="6"
-          placeholder="password"
-          ref={passwordRef}
-        />
-        <button className="registerBtn" type="submit">
-          Register
-        </button>
-        {success && (
-          <span className="success">Successfull. You can login now!</span>
-        )}
-        {error && <span className="failure">Something went wrong!</span>}
-      </form>
-      <Cancel
-        className="registerCancel"
-        onClick={() => setShowRegister(false)}
-      />
+    <div className='registerContainer'>
+        <div className='logo'>
+          <Room/>
+          LabibPin
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder='username' ref={nameRef}/>
+          <input type="text" placeholder='email' ref={emailRef}/>
+           <input type="password" placeholder='password' ref={passwordRef}/>
+           <button className='registerBtn'>Register</button>
+           {success && (
+            <span className='success'>Successfull.You can login now!</span>
+           )}
+           {error && (
+               <span className='failure'>Something went wrong!</span>
+           )}
+          <Cancel className='registerCancel'  onClick={()=>setShowRegister(false)}/>
+        </form>
     </div>
-  );
+  )
 }
